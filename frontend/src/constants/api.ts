5a583eb44efc70c3f5ac5c483068b72c4d93b597
Jiyaho <masqu2n@gmail.com>
2023-11-15 00:18:10 +0900

    Chore: 카카오 로그인 관련 변수 선언

--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -1,3 +1,7 @@
 export const END_POINT = {
   USER_DETAIL: (userId: string) => `api/user/${userId}`,
 } as const;
+
+export const KAKAO_CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID; // REST_API_KEY
+export const KAKAO_LOGOUT_REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_LOGOUT_REDIRECT_URI;
+export const KAKAO_AUTH_LOGOUT_URI = `https://kauth.kakao.com/oauth/logout?client_id=${KAKAO_CLIENT_ID}&logout_redirect_uri=${KAKAO_LOGOUT_REDIRECT_URI}`;

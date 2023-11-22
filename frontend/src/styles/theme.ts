b0a090c94af86d8716fbb01e6a022c45c2ee9dc0
Steadystudy <sgmin206@naver.com>
2023-11-22 20:26:51 +0900

    Feat: Theme

---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,20 @@
+const Theme = {
+  colors: {
+    black: '#000000',
+    white: '#FFFFFF',
+    gray100: '#F2F2F2',
+    gray200: '#E0E0E0',
+    gray300: '#BDBDBD',
+    gray400: '#828282',
+    gray500: '#4F4F4F',
+    gray600: '#333333',
+  },
+  fontSize: {
+    large: '1.25rem',
+    medium: '1rem',
+    small: '0.875rem',
+    xsmall: '0.75rem',
+  },
+};
+
+export default Theme;

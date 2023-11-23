fc5358033476d6d7c3bb94c932373dae80cb17ed
Steadystudy <sgmin206@naver.com>
2023-11-23 16:09:55 +0900

    Feat: Calendar 컴포넌트 및 useCalendar 커스텀 훅

---
+++ frontend/src/components/Calendar/Calendar.style.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import Theme from '../../styles/theme';
+import { DayDefaultBox } from './Day/Day.style';
+
+export const CalendarContainer = styled.div`
+  display: inline-block;
+`;
+
+export const CalendarHeader = styled.header`
+  margin-bottom: 6px;
+  text-align: center;
+
+  & div {
+    font-weight: 600;
+  }
+`;
+
+export const WeekOfDaysBox = styled(DayDefaultBox)`
+  color: ${Theme.colors.gray400};
+
+  font-size: ${Theme.fontSize.xsmall};
+  font-weight: 400;
+`;
+
+export const DaysContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(7, 48px);
+  grid-auto-rows: 48px;
+  border-spacing: 0 2px;
+  grid-row-gap: 1px;
+`;
+
+export const DaysOfWeekContainer = styled(DaysContainer)`
+  margin-bottom: 2px;
+
+  & div {
+    color: ${Theme.colors.gray600};
+
+    cursor: default;
+  }
+`;
---
+++ frontend/src/components/Calendar/Day/Day.style.ts
@@ -0,0 +1,73 @@
+import Theme from '../../../styles/theme';
+import styled from 'styled-components';
+
+export const DayContainer = styled.div<{
+  $isStart?: boolean;
+  $isEnded?: boolean;
+  $isInRange?: boolean;
+}>`
+  min-width: 48px;
+  width: 48px;
+  height: 48px;
+
+  background-color: ${(props) => (props.$isInRange ? Theme.colors.gray100 : Theme.colors.white)};
+
+  border-top-left-radius: ${(props) => (props.$isStart ? '50%' : '')};
+  border-bottom-left-radius: ${(props) => (props.$isStart ? '50%' : '')};
+
+  border-top-right-radius: ${(props) => (props.$isEnded ? '50%' : '')};
+  border-bottom-right-radius: ${(props) => (props.$isEnded ? '50%' : '')};
+`;
+
+export const DayDefaultBox = styled.span<{ $isClickable?: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 46px;
+  height: 46px;
+  background-color: ${Theme.colors.white};
+  box-sizing: border-box;
+
+  font-size: ${Theme.fontSize.small};
+  font-weight: 600;
+
+  cursor: ${(props) => (props.$isClickable ? 'pointer' : 'default')};
+  border-radius: 50%;
+
+  &:hover {
+    border: ${(props) => (props.$isClickable ? `0.5px solid ${Theme.colors.black}` : '')};
+  }
+`;
+
+export const TodayBox = styled(DayDefaultBox)`
+  background-color: ${Theme.colors.blue300};
+
+  color: ${Theme.colors.blue800};
+
+  &:hover {
+    background-color: ${Theme.colors.blue300};
+  }
+`;
+
+export const DayInRangeBox = styled(DayDefaultBox)`
+  background-color: inherit;
+`;
+
+export const SelectedDayBox = styled(DayDefaultBox)`
+  background-color: ${Theme.colors.black};
+
+  color: ${Theme.colors.white};
+`;
+
+export const DisabledDayBox = styled(DayDefaultBox)`
+  background-color: ${Theme.colors.white};
+
+  color: ${Theme.colors.gray300};
+
+  pointer-events: none;
+
+  &:hover {
+    background-color: ${Theme.colors.white};
+  }
+`;

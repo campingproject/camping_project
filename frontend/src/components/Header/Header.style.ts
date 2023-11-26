58a9e27b622067b4f3308a329f5b1a7a9f279982
Steadystudy <sgmin206@naver.com>
2023-11-27 01:02:03 +0900

    Feat: Header 컴포넌트 뼈대 구성

---
+++ frontend/src/components/Header/Header.style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,5 +1,18 @@
+'use client';
+
+import Link from 'next/link';
 import { Logo } from '../Logo';
+import { SearchBar } from '../SearchBar';
+import { HeaderContainer } from './Header.style';
 
 export default function Header() {
-  return <Logo />;
+  return (
+    <HeaderContainer>
+      <Link href={'/'}>
+        <Logo />
+      </Link>
+      <SearchBar />
+      <div>profile</div>
+    </HeaderContainer>
+  );
 }

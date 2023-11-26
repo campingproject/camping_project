948b6b86241c60fc33c1334e67faa3f4e09d50d6
Steadystudy <sgmin206@naver.com>
2023-11-27 00:56:15 +0900

    Feat: SearchBar Dialog 컴포넌트

---
+++ frontend/src/components/SearchBarDialog/SearchBarDialog.style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Dialog = styled.dialog`
+  position: absolute;
+  top: 100%;
+  z-index: 1;
+
+  display: block;
+  padding: 24px;
+  border: 1px solid black;
+  border-radius: 24px;
+  margin-top: 12px;
+`;

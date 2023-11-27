4af7e0fc898e3f122b448306b14a8c526a2fb278
Jiyaho <masqu2n@gmail.com>
2023-11-28 03:00:24 +0900

    Feat: 로그인 리다이렉트 페이지

--- frontend/src/app/login/oauth2/code/[...id]/page.tsx
+++ frontend/src/app/login/oauth2/code/[...id]/page.tsx
@@ -1,20 +1,34 @@
 'use client';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import axios from 'axios';
 
-function OauthRedirectHandler() {
+function OauthRedirectedPage() {
   const router = useRouter();
-  useEffect(() => {
-    const kakaoRedirectUrl = sessionStorage.getItem('kakaoRedirectUrl');
-    if (kakaoRedirectUrl) {
-      // 인가 코드
-      const code = new URL(window.location.href).searchParams.get('code');
-      console.log(code);
-      // login process
-      router.push(kakaoRedirectUrl);
-    }
-  });
+  // const code = new URL(window.location.href).searchParams.get('code');
+  const handleKakaoLogin = async () => {
+    try {
+      // 서버로 API 요청을 보냄
+      const response = await axios.get('/api/login', {
+        params: {
+          code: new URL(window.location.href).searchParams.get('code'),
+        },
+      });
 
-  return <div>OauthRedirectHandler</div>;
+      // 서버에서 처리된 데이터를 사용
+      console.log(response.data);
+
+      // 로그인 성공 시 메인(홈) 화면으로 이동
+      router.push('/');
+    } catch (error) {
+      console.error('Error during login:', error);
+      // 오류 처리
+    }
+  };
+  useEffect(() => {
+    handleKakaoLogin();
+  }, []);
+
+  return <div>OauthRedirectedPage... 로그인 중입니다.</div>;
 }
-export default OauthRedirectHandler;
+export default OauthRedirectedPage;

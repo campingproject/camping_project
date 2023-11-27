d0b79d0fb22fda3848929aceb70313ab2b889a65
Jiyaho <masqu2n@gmail.com>
2023-11-28 03:00:24 +0900

    Feat: 로그인 리다이렉트 페이지

--- frontend/src/app/login/oauth2/code/[...id]/page.tsx
+++ frontend/src/app/login/oauth2/code/[...id]/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
-import axios from 'axios';
 
 function OauthRedirectedPage() {
   const router = useRouter();
@@ -9,14 +8,14 @@ function OauthRedirectedPage() {
   const sendAuthCode = async () => {
     try {
       // 서버로 API 요청을 보냄
-      const response = await axios.get('/api/login', {
-        params: {
-          code: new URL(window.location.href).searchParams.get('code'),
-        },
-      });
+      // const response = await axios.get('/api/login', {
+      //   params: {
+      //     code: new URL(window.location.href).searchParams.get('code'),
+      //   },
+      // });
 
       // 서버에서 처리된 데이터를 사용
-      console.log(response.data);
+      // console.log(response.data);
 
       // 로그인 성공 시 메인(홈) 화면으로 이동
       router.push('/');

66b8f485b24f78270007e76bc2447fbd1267b70a
Jiyaho <masqu2n@gmail.com>
2023-12-12 20:13:34 +0900

    Feat: 캠핑장 상세보기 페이지 및 컴포넌트 생성

--- frontend/src/app/places/[id]/page.tsx
+++ frontend/src/app/places/[id]/page.tsx
@@ -1,13 +1,25 @@
+'use client';
 import { Header } from '@/components/Header';
 import PlaceDetails from '@/components/elements/PlaceDetails';
+import { useParams } from 'next/navigation';
+import useSWR from 'swr';
 
 function PlaceDetailPage() {
+  const params = useParams();
+  const placeId = params.id;
+
+  const { data, isLoading, error } = useSWR(`/api/places/${placeId}`);
+
+  if (error) {
+    console.log('Fetch Error: ' + error);
+    return <div>Error fetching data</div>;
+  }
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <>
       <Header />
-      <main>
-        <PlaceDetails />
-      </main>
+      <PlaceDetails data={data} />
     </>
   );
 }
--- frontend/src/components/elements/PlaceDetails/PlaceDetails.tsx
+++ frontend/src/components/elements/PlaceDetails/PlaceDetails.tsx
@@ -1,40 +1,41 @@
 'use client';
-import { placeSampleImg1, placeSampleImg2 } from '@/public/svgs';
-import PlaceDescriptions from './fragments/PlaceDescriptions';
-import PlaceImages from './fragments/PlaceImages';
 import { useState } from 'react';
-import styled from 'styled-components';
-import PlaceImageModal from './fragments/PlaceImageModal';
+import PlaceDescriptions from './fragments/PlaceDescriptions';
+import PlaceImagesModal from './fragments/PlaceImagesModal';
+import { ButtonStyled, ImagesWrap, PlaceDetailContainer } from './PlaceDetail.styles';
+import Image from 'next/image';
+import { PlaceData } from '@/types/place';
 
-function PlaceDetails() {
-  const [modalOpen, setModalOpen] = useState(false);
-  const openModal = () => setModalOpen(true);
-  const closeModal = () => setModalOpen(false);
-  const images = [placeSampleImg1, placeSampleImg2];
+interface PlaceDatilsProps {
+  data: PlaceData;
+}
+
+function PlaceDetails({ data }: PlaceDatilsProps) {
+  const [viewAllImages, setViewAllImages] = useState(false);
+
+  // 사진 모두 보기 클릭한 경우 Modal Open
+  const openModal = () => setViewAllImages(true);
+  const closeModal = () => setViewAllImages(false);
+
+  // 캠핑장 대표 이미지 3개만 출력
+  const mainImages = data?.images.slice(0, 3);
 
   return (
-    <>
-      <PlaceImages modalOpen={modalOpen} onClick={openModal} />
-      <ButtonStyled style={{ display: modalOpen ? 'none' : 'block' }} onClick={openModal}>
-        사진 모두 보기
-      </ButtonStyled>
-      {modalOpen && <PlaceImageModal images={images} closeModal={closeModal} />}
-      <PlaceDescriptions modalOpen={modalOpen} />
+    <PlaceDetailContainer>
+      <ImagesWrap onClick={openModal} style={{ display: viewAllImages ? 'none' : 'grid' }}>
+        {mainImages.map((image, i) => (
+          <Image src={image} alt={image} width={0} height={0} key={i} priority />
+        ))}
+        <ButtonStyled style={{ display: viewAllImages ? 'none' : 'block' }} onClick={openModal}>
+          사진 모두 보기
+        </ButtonStyled>
+      </ImagesWrap>
+      {viewAllImages && <PlaceImagesModal images={data?.images} closeModal={closeModal} />}
+      <PlaceDescriptions viewAllImages={viewAllImages} placeData={data} />
 
-      <section style={{ display: modalOpen ? 'none' : 'block' }}>예약 정보 컴포넌트</section>
-    </>
+      {/* <section style={{ display: viewAllImages ? 'none' : 'block' }}>예약 정보 컴포넌트</section> */}
+    </PlaceDetailContainer>
   );
 }
-export default PlaceDetails;
 
-export const ButtonStyled = styled.button`
-  position: absolute;
-  top: 77vh;
-  left: 79vw;
-  border-radius: 15px;
-  border: none;
-  width: 110px;
-  height: 33px;
-  font-weight: bold;
-  cursor: pointer;
-`;
+export default PlaceDetails;
